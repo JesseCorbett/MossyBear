913afa36428e2cfe1107b0ec1b1cfa39bcb3d238
Jesse Corbett <jesselcorbett@gmail.com>
2024-07-18 11:02:06 -0700

    Fix a bit o logic

--- src/components/StatSet.vue
+++ src/components/StatSet.vue
@@ -35,7 +35,11 @@ const characterStore = useCharacterStore()
 async function rollStat(stat: number = 0, label: string = props.label) {
   const results = Array.from({ length: model.value.length + stat }, rollDie)
   let total = 0
-  results.forEach((die) => (total < die) ? total = die)
+  results.forEach((die) => {
+    if (die > total) {
+      total = die
+    }
+  })
   await OBR.notification.show(`${characterStore.playerName} rolled a ${total} for ${label}`)
 }
 
