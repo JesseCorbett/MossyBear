4b368103cee0cf3a8a13b2630c17bf45b1f62536
Jesse Corbett <jesselcorbett@gmail.com>
2024-07-29 16:52:53 -0700

    Add inventory

--- src/views/CharacterGuns.vue
+++ src/views/CharacterGuns.vue
@@ -3,6 +3,10 @@
     <GunSet v-model="sheet.primary"/>
     <GunSet v-model="sheet.secondary"/>
     <GunSet v-model="sheet.heavy"/>
+    <div id="inventory">
+      <label for="inventory-input">Inventory</label>
+      <textarea v-model="sheet.inventory" id="inventory-input"></textarea>
+    </div>
   </div>
 </template>
 
@@ -21,5 +25,29 @@ const sheet = defineModel<CharacterSheet>({
   grid-template-columns: repeat(auto-fit, minmax(325px, 1fr));
   grid-template-rows: auto;
   gap: 32px 16px;
+}
+
+#inventory {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+#inventory label {
+  display: block;
+  margin: 0 0 0 auto;
+  background: var(--theme-secondary);
+  padding: 6px 12px;
+  font-size: 18px;
+  font-weight: 600;
+}
+
+#inventory textarea {
+  display: block;
+  background: var(--theme-secondary);
+  width: 100%;
+  flex: 1;
+  min-height: 120px;
+  box-sizing: border-box;
 }
 </style>
